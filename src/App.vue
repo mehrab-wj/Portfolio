8e458a99d47caf9a64137cf12c878c5cef6451be
mehrab-wj <white_joker20@yahoo.com>
2021-11-23 20:35:22 +0330

    initialize Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Navbar />
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+export default {
+  Name: "App",
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      name: "Mehrab",
+      full_name: "Mehrab Hojjati Pour"
+    }
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  -moz-osx-font-smoothing: grayscale; 
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="flex justify-between pt-6 px-4">
+        <h1 class='font-bold text-xl'>Mehrab<small class='text-yellow-500'>.</small></h1>
+
+        <ul class='flex space-x-6'>
+            <li>Portfolio</li>
+            <li>About me</li>
+            <li>Skills</li>
+            <li>Contact me</li>
+        </ul>
+
+        <div>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+    </nav>
+</template>
