c41a77330d0e1a2ce3cb0de97694fc0676b10623
mehrab-wj <white_joker20@yahoo.com>
2021-11-23 21:57:04 +0330

    make navbar social links more dynamic

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
     <nav class="flex justify-between pt-4 px-4 items-baseline">
-        <h1 class='font-bold text-xl'>{{this.data.name}}<label class='text-yellow-400 text-3xl'>.</label></h1>
+        <h1 class="font-bold text-xl">
+            {{ this.data.name
+            }}<label class="text-yellow-400 text-3xl">.</label>
+        </h1>
 
-        <ul class='flex space-x-10'>
+        <ul class="flex space-x-10">
             <li><a class="active" href="#">Portfolio</a></li>
             <li><a class="" href="#">About me</a></li>
             <li><a class="" href="#">Skills</a></li>
@@ -10,7 +13,13 @@
         </ul>
 
         <div>
-            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a
+                :key="social.key"
+                v-for="social in this.data.social"
+                :href="social.link"
+                target="_blank"
+                ><i :class="social.icon" class="ml-3 transition duration-200 text-md"></i
+            ></a>
         </div>
     </nav>
 </template>
@@ -18,15 +27,15 @@
 <script>
 export default {
     name: "Navbar",
-    props: ['data']
-}
+    props: ["data"],
+};
 </script>
 
 <style lang="postcss" scoped>
-    nav ul li a {
-        @apply cursor-pointer pb-1 hover:text-yellow-400 border-b-2 border-transparent hover:border-yellow-400 transition duration-200;
-    }
-    nav ul li a.active {
-        @apply text-yellow-400 border-yellow-400;
-    }
+nav ul li a {
+    @apply cursor-pointer pb-1 hover:text-yellow-400 border-b-2 border-transparent hover:border-yellow-400 transition duration-200;
+}
+nav ul li a.active {
+    @apply text-yellow-400 border-yellow-400;
+}
 </style>
