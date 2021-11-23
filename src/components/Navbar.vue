9923d22ad7b213b754607b906d29c521a34a2a57
mehrab-wj <white_joker20@yahoo.com>
2021-11-23 23:31:36 +0330

    track active state in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <nav class="flex justify-between pt-4 px-4 items-baseline">
+    <nav class="flex justify-between pt-4 px-14 items-baseline z-50">
         <h1 class="font-bold text-xl">
             {{ this.data.name
             }}<label class="text-yellow-400 text-3xl">.</label>
         </h1>
 
         <ul class="flex space-x-10">
-            <li><a class="active" href="#">Portfolio</a></li>
-            <li><a class="" href="#">About me</a></li>
-            <li><a class="" href="#">Skills</a></li>
-            <li><a class="" href="#">Contact me</a></li>
+            <li><router-link to="/" >Portfolio</router-link></li>
+            <li><router-link to="/about" >About me</router-link></li>
+            <li><router-link to="/skills" >Skills</router-link></li>
+            <li><router-link to="/contact" >Contact me</router-link></li>
         </ul>
 
         <div>
@@ -27,7 +27,13 @@
 <script>
 export default {
     name: "Navbar",
-    props: ["data"],
+    props: ["data", 'currentRoute'],
+
+    computed: {
+        activeRoute() {
+            return this.$route.path === this.currentRoute;
+        }
+    }
 };
 </script>
 
@@ -35,7 +41,7 @@ export default {
 nav ul li a {
     @apply cursor-pointer pb-1 hover:text-yellow-400 border-b-2 border-transparent hover:border-yellow-400 transition duration-200;
 }
-nav ul li a.active {
+nav ul li a.router-link-active, nav ul li a.router-link-exact-activ {
     @apply text-yellow-400 border-yellow-400;
 }
 </style>
