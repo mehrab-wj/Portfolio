d0f285002959dd7964a67e39a0e69a73a9acc499
mehrab-wj <white_joker20@yahoo.com>
2021-11-25 19:21:09 +0330

    animate navbar entrace

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ export default {
         return {
             currentRoute: window.location.pathname,
             data: {
-                transitionName: "fade",
                 primaryColor: "#FBBF24",
                 name: "Mehrab",
                 full_name: "Mehrab Hojjati Pour",
@@ -80,6 +79,19 @@ export default {
 .fade-enter-from,
 .fade-leave-to {
     opacity: 0;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
 }
 .-z-1 {
     z-index: -1;
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,12 +3,11 @@
         <h1 class="font-bold text-xl">
             {{ this.data.name }}<label class="text-3xl">.</label>
         </h1>
-
         <div
             id="navbar-items"
             :class="[
                 menuOpen
-                    ? 'rounded-md flex justify-center flex-wrap absolute top-11 shadow-lg p-6 w-full bg-white'
+                    ? 'slide-in-top rounded-md flex justify-center flex-wrap absolute top-11 shadow-lg p-6 w-full bg-white'
                     : 'hidden md:flex lg:flex justify-between flex-grow',
             ]"
         >
@@ -19,12 +18,18 @@
                         : 'flex space-x-4 lg:space-x-10 mx-auto'
                 "
             >
-                <li @click="toggleMenu(true)"><router-link to="/">Portfolio</router-link></li>
-                <li @click="toggleMenu(true)"><router-link to="/about">About me</router-link></li>
+                <li @click="toggleMenu(true)">
+                    <router-link to="/">Portfolio</router-link>
+                </li>
+                <li @click="toggleMenu(true)">
+                    <router-link to="/about">About me</router-link>
+                </li>
                 <li @click="toggleMenu(true)">
                     <router-link to="/projects">Projects overview</router-link>
                 </li>
-                <li @click="toggleMenu(true)"><router-link to="/contact-me">Contact me</router-link></li>
+                <li @click="toggleMenu(true)">
+                    <router-link to="/contact-me">Contact me</router-link>
+                </li>
             </ul>
 
             <div
@@ -46,8 +51,11 @@
                 ></a>
             </div>
         </div>
-
-        <button id='mobileMenuBtn' @click="toggleMenu()" class="text-2xl md:hidden transition duration-500">
+        <button
+            id="mobileMenuBtn"
+            @click="toggleMenu()"
+            class="text-2xl md:hidden transition duration-500"
+        >
             <i :class="mobileMenuIcon"></i>
         </button>
     </nav>
@@ -60,31 +68,28 @@ export default {
     data() {
         return {
             menuOpen: false,
-            mobileMenuIcon: "fas fa-bars"
+            mobileMenuIcon: "fas fa-bars",
         };
     },
     methods: {
         toggleMenu(forceClose = false) {
-            this.menuOpen = (forceClose ? false : !this.menuOpen);
+            this.menuOpen = forceClose ? false : !this.menuOpen;
             if (this.menuOpen) {
-                this.mobileMenuIcon = 'fas fa-times opacity-20';
-                document.querySelector("#mobileMenuBtn").style.transform = "rotate(360deg)";
+                this.mobileMenuIcon = "fas fa-times opacity-20";
+                document.querySelector("#mobileMenuBtn").style.transform =
+                    "rotate(360deg)";
                 return;
             }
-            this.mobileMenuIcon = 'fas fa-bars';
-            document.querySelector("#mobileMenuBtn").style.transform = "rotate(0deg)";
+            this.mobileMenuIcon = "fas fa-bars";
+            document.querySelector("#mobileMenuBtn").style.transform =
+                "rotate(0deg)";
         },
-        
     },
     computed: {},
 };
 </script>
 
 <style lang="postcss" scoped>
-.rotate-center {
-	-webkit-animation: rotate-center 0.4s ease-in-out both;
-	        animation: rotate-center 0.4s ease-in-out both;
-}
 nav h1 label,
 .fa-bars {
     color: var(--primary-color);
@@ -98,25 +103,68 @@ nav ul li a.router-link-exact-activ {
     color: var(--primary-color);
     border-color: var(--primary-color);
 }
-@-webkit-keyframes rotate-center {
+.slide-in-top {
+	-webkit-animation: slide-in-top 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+	        animation: slide-in-top 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+.scale-out-ver-top {
+	-webkit-animation: scale-out-ver-top 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+	        animation: scale-out-ver-top 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+}
+@-webkit-keyframes scale-out-ver-top {
   0% {
-    -webkit-transform: rotate(0);
-            transform: rotate(0);
+    -webkit-transform: scaleY(1);
+            transform: scaleY(1);
+    -webkit-transform-origin: 100% 0%;
+            transform-origin: 100% 0%;
+    opacity: 1;
   }
   100% {
-    -webkit-transform: rotate(360deg);
-            transform: rotate(360deg);
+    -webkit-transform: scaleY(0);
+            transform: scaleY(0);
+    -webkit-transform-origin: 100% 0%;
+            transform-origin: 100% 0%;
+    opacity: 1;
   }
 }
-@keyframes rotate-center {
+@keyframes scale-out-ver-top {
   0% {
-    -webkit-transform: rotate(0);
-            transform: rotate(0);
+    -webkit-transform: scaleY(1);
+            transform: scaleY(1);
+    -webkit-transform-origin: 100% 0%;
+            transform-origin: 100% 0%;
+    opacity: 1;
   }
   100% {
-    -webkit-transform: rotate(360deg);
-            transform: rotate(360deg);
+    -webkit-transform: scaleY(0);
+            transform: scaleY(0);
+    -webkit-transform-origin: 100% 0%;
+            transform-origin: 100% 0%;
+    opacity: 1;
   }
 }
-
+@-webkit-keyframes slide-in-top {
+  0% {
+    -webkit-transform: translateY(-1000px);
+            transform: translateY(-1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-top {
+  0% {
+    -webkit-transform: translateY(-1000px);
+            transform: translateY(-1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+    opacity: 1;
+  }
+}
 </style>
