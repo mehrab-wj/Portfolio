cd5acdbc9715ba1069ee67a9ac29dc66f1687c7f
mehrab-wj <white_joker20@yahoo.com>
2021-11-25 18:14:53 +0330

    basic responsive for Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,55 @@
 <template>
-    <nav class="flex justify-between items-baseline z-50">
+    <nav class="flex justify-between items-baseline z-50 relative">
         <h1 class="font-bold text-xl">
             {{ this.data.name }}<label class="text-3xl">.</label>
         </h1>
 
-        <ul class="flex space-x-10">
-            <li><router-link to="/">Portfolio</router-link></li>
-            <li><router-link to="/about">About me</router-link></li>
-            <li><router-link to="/projects">Projects overview</router-link></li>
-            <li><router-link to="/contact-me">Contact me</router-link></li>
-        </ul>
+        <div
+            id="navbar-items"
+            :class="[
+                menuOpen
+                    ? 'rounded-md flex justify-center flex-wrap absolute top-11 shadow-lg p-6 w-full bg-white'
+                    : 'hidden md:flex lg:flex justify-between flex-grow',
+            ]"
+        >
+            <ul
+                :class="
+                    menuOpen
+                        ? 'w-full text-center space-y-3'
+                        : 'flex space-x-4 lg:space-x-10 mx-auto'
+                "
+            >
+                <li><router-link to="/">Portfolio</router-link></li>
+                <li><router-link to="/about">About me</router-link></li>
+                <li>
+                    <router-link to="/projects">Projects overview</router-link>
+                </li>
+                <li><router-link to="/contact-me">Contact me</router-link></li>
+            </ul>
 
-        <div>
-            <a
-                :key="social.key"
-                v-for="social in this.data.social"
-                :href="social.link"
-                target="_blank"
-                ><i
-                    :class="social.icon"
-                    class="ml-3 transition duration-200 text-md"
-                ></i
-            ></a>
+            <div
+                :class="
+                    menuOpen
+                        ? 'w-full mt-6 text-center space-x-4'
+                        : 'space-x-3 lg:space-x-5'
+                "
+            >
+                <a
+                    :key="social.key"
+                    v-for="social in this.data.social"
+                    :href="social.link"
+                    target="_blank"
+                    ><i
+                        :class="social.icon"
+                        class="transition duration-200 text-lg"
+                    ></i
+                ></a>
+            </div>
         </div>
+
+        <button @click="toggleMenu" class="text-2xl md:hidden">
+            <i class="fas fa-braille"></i>
+        </button>
     </nav>
 </template>
 
@@ -30,13 +57,23 @@
 export default {
     name: "Navbar",
     props: ["data"],
-
+    data() {
+        return {
+            menuOpen: false,
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.menuOpen = !this.menuOpen;
+        },
+    },
     computed: {},
 };
 </script>
 
 <style lang="postcss" scoped>
-nav h1 label {
+nav h1 label,
+.fa-braille {
     color: var(--primary-color);
 }
 nav ul li a {
